b08452c5e7da66e6b846d3d450c260c49d64909a
connellyw <connellyw@allegheny.edu>
2020-10-04 15:22:59 -0400

    Update compareCountries_17Sept2020.R

--- lessons/03_week_visualizing/sandbox/coronaVirusAnalysis/compareCountries_17Sept2020.R
+++ lessons/03_week_visualizing/sandbox/coronaVirusAnalysis/compareCountries_17Sept2020.R
@@ -1,40 +1,29 @@
 # Date: 17 Sept 2020
 # What is this? Code to track the infections of COVID-19 for comparison between countries
 
-rm(list = ls()) # clear out the variables in memory
-
-
-# install the libraries if they are not already on your machine.
-install.packages("tidyverse")
-install.packages("plotly")
+rm(list = ls()) # clear out the variables to makes a clean run
 
 library(tidyverse)
 library(plotly)
-
-
 # note: set your local directory to the one that contains the data file.
 # code ref: https://community.rstudio.com/t/loading-an-rda-file-into-r/45984/4
-
 # data ref: https://github.com/RamiKrispin/coronavirus/blob/master/data/coronavirus.rda
-# Use GitHub's DOWNLOAD button to get the latest version of the data.
 
-# is the file is in the working directory.
-#dataFile <- "coronavirus.rda"
-#load(dataFile)
+dataFile <- "coronavirus.rda"
+load(dataFile)
 
 
-# If you have to search for the file, then use this code.
+# if file is not in the same local directory as this code
 # file.exists(dataFile)
-myFile <- file.choose()
-load(myFile)
+ myFile <- file.choose(dataFile)
+ load(myFile)
 
-
-
-# What countries do I have data for analysis?
-unique(coronavirus$country)
 
 # What providences do I have data for analysis?
 unique(coronavirus$province)
+
+# What countries do I have data for analysis?
+unique(coronavirus$country)
 
 # View the data
 view(coronavirus)
@@ -42,7 +31,7 @@ view(coronavirus)
 
 
 
-#### Plotting data ####
+#### Data ####
 
 # ensure all country data is same length by pulling data from same dataset.
 # isolate all the case and date information to create smaller tables that we will use.
@@ -55,7 +44,7 @@ view(coronavirus)
 # Sweden
 #### #### #### #### #### #### 
 
-swedenDat <- coronavirus %>% filter(country == "Sweden", province =="", type == "confirmed") %>% select(date,cases,type)
+swedenDat <- coronavirus %>% filter(country == "Sweden", type == "confirmed") %>% select(date,cases,type)
 names(swedenDat) <- c("swedenDate","swedenCases")
 
 ggplot(swedenDat, aes(swedenDate, swedenCases)) +  geom_line(color = "red" ) + ylab("Sweden Cumulative confirmed cases") 
@@ -67,7 +56,7 @@ ggplot(swedenDat, aes(swedenDate, swedenCases)) +  geom_line(color = "red" ) + y
 #### #### #### #### #### #### 
 
 
-franceDat <- coronavirus %>% filter(country == "France", province =="", type == "confirmed") %>% select(date,cases,type)
+franceDat <- coronavirus %>% filter(country == "France", type == "confirmed") %>% select(date,cases,type)
 names(franceDat) <- c("franceDate","franceCases")
 
 ggplot(franceDat, aes(franceDate, franceCases)) +  geom_line(color = "red" ) + ylab("France Cumulative confirmed cases") 
@@ -79,7 +68,7 @@ ggplot(franceDat, aes(franceDate, franceCases)) +  geom_line(color = "red" ) + y
 #### #### #### #### #### #### 
 
 
-usDat <- coronavirus %>% filter(country == "US", province =="", type == "confirmed") %>% select(date,cases,type)
+usDat <- coronavirus %>% filter(country == "US", type == "confirmed") %>% select(date,cases,type)
 names(usDat) <- c("usDate","usCases")
 
 ggplot(usDat, aes(usDate, usCases)) +  geom_line(color = "red" ) + ylab("US Cumulative confirmed cases") 
@@ -89,7 +78,7 @@ ggplot(usDat, aes(usDate, usCases)) +  geom_line(color = "red" ) + ylab("US Cumu
 # Italy
 #### #### #### #### #### #### 
 
-italyDat <- coronavirus %>% filter(country == "Italy", province =="", type == "confirmed") %>% select(date,cases,type)
+italyDat <- coronavirus %>% filter(country == "Italy", type == "confirmed") %>% select(date,cases,type)
 names(italyDat) <- c("italyDate","italyCases")
 
 ggplot(italyDat, aes(italyDate, italyCases)) +  geom_line(color = "red" ) + ylab("Italy Cumulative confirmed cases") 
@@ -116,20 +105,20 @@ trace_1 <- c(franceDat$franceCases)
 trace_2 <- c(usDat$usCases)
 trace_3 <- c(italyDat$italyCases)
 
-
 # Establish the x-axis
-x <- c(1:length(trace_0)) # need an x-axis so we will use the dates, note all these traces should be the same length.
+x <- c(1:length(trace_0)) # need an x-axis so we will use the dates.
+x <- c(usDat$usDate) # need an x-axis so we will use the dates.
 
 # add the traces from each country to the main plot's data set
-#myData <- data.frame(x, trace_0, trace_1, trace_2, trace_3, trace_4)
 myData <- data.frame(x, trace_0, trace_1, trace_2, trace_3)
 
 
 # the code to create the plot from the individual traces
 p <- plot_ly(myData, x = ~x, y = ~trace_0, name = "Sweden", type = 'scatter', mode = 'markers') %>% 
   add_trace( x = ~x,y = ~trace_1, name = "France", type = 'scatter', mode = 'markers') %>% 
-  add_trace( x = ~x, y = ~trace_2, name = "US", type = 'scatter', mode = 'markers') %>%
-  add_trace( x = ~x, y = ~trace_3, name = "Italy", type = 'scatter', mode = 'markers')
+  add_trace( x = ~x, y = ~trace_2, name = "US",     type = 'scatter', mode = 'markers') %>%
+  add_trace( x = ~x, y = ~trace_3, name = "Italy",     type = 'scatter', mode = 'markers')
 
 # show the plot
 p
+
